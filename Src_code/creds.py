aec90bb21897a55836f642f6affe7cd4534ccae7
ANRD7 <anirudh7979@gmail.com>
2023-08-03 19:24:05 +0530

    name update

--- Src_code/creds.py
+++
@@ -1,7 +0,0 @@
-def mysql_creds():
-    host="interndeveloper.cgzn4uluwtm1.us-east-2.rds.amazonaws.com"
-    dbase="interndev"
-    user="admin"
-    password="Dinshu7951"
-    port=3306
-    return host,dbase,user,password,port
